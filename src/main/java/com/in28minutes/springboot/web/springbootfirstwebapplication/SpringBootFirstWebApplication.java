6e5dbb83faf54e8e6e8c0644d636ce55b487e87b
ahmedyasser170 <40868178+ahmedyasser170@users.noreply.github.com>
2022-04-18 03:45:34 +0200

    test commit

--- src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/SpringBootFirstWebApplication.java
+++ src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/SpringBootFirstWebApplication.java
@@ -3,7 +3,7 @@ package com.in28minutes.springboot.web.springbootfirstwebapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.in28minutes.springboot.web"})
+	@SpringBootApplication(scanBasePackages={"com.in28minutes.springboot.web"})
 public class SpringBootFirstWebApplication {
 
 	public static void main(String[] args) {
